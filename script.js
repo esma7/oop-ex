22d0b3b5c853567776c14897e0f6e398796e31de
esma3 <semababayeva9898@gmail.com>
2024-08-18 11:53:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//  new task:
+// Person classi yaradib ona name age ve surname verin daha sonra onun doğum ilini hesablayan class icinde bir function yazin
+// Meselen age 20 verdiyimizde function 2004 nəticəsini versin
+
+
+// class Person{
+
+//     constructor(name,age,surname){
+//         this.name = name 
+//         this.age = age
+//         this.surname = surname
+//     }
+
+//     birthday(){
+//         const birthday = 2024 - this.age
+//         console.log(`${this.name}, ${this.surname}, tevelludum - ${birthday} `);
+        
+//     }
+
+// }
+
+// const person1 = new Person("Sema", 15, "Babayeva")
+// person1.birthday()
+// console.log(person1.age);
+
+/////////diger yol
+
+// class Person{
+
+//     constructor(name,age,surname){
+//         this.birthday(name, age, surname)
+//     }
+
+//     birthday(name,age,surname){
+  
+//         this.name = name 
+//         this.age = age
+//         this.surname = surname
+//         const birthday = 2024 - this.age
+//         console.log(`${this.name}, ${this.surname}, tevelludum - ${birthday} `);
+        
+//     }
+
+// }
+
+// const person1 = new Person("Sema", 15, "Babayeva")
+
+// console.log(person1.age);
+
+
+
+
+
+
